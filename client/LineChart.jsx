c7545ae9afe7b847da65a08935396dfc51198bde
Evelyn Chen <evelync93@gmail.com>
2019-10-15 14:55:32 -0400

    change curved line graph to straight lines

--- client/LineChart.jsx
+++ client/LineChart.jsx
@@ -16,39 +16,30 @@ export default class LineChart extends Component {
             {
               label: "Closing Price",
               data: [
-                // {
-                //   x: "1980-01-01",
-                //   y: 10
-                // },
-                // {
-                //   x: "1980-06-01",
-                //   y: 5
-                // },
-                // {
-                //   x: "1981-01-01",
-                //   y: 20
-                // }
                 { x: "2019-01-01", y: 3869.47 },
                 { x: "2019-01-02", y: 3941.2167 },
                 { x: "2019-01-03", y: 3832.155 },
-                {
-                  x: "2019-01-04",
-                  y: 3863.6267
-                },
+                { x: "2019-01-04", y: 3863.6267 },
                 { x: "2019-01-05", y: 3835.5983 }
               ]
             }
           ]
         },
       options: {
+        // elements: {
+        //   line: {
+        //     tension: 0
+        //   }
+        // },
+        bezierCurve: false,
         scales: {
           xAxes: [
             {
               type: "time",
               distribution: "series",
-              // time: {
-              //   unit: "day"
-              // },
+              time: {
+                unit: "day"
+              },
               ticks: {
                 source: "data",
                 autoSkip: true
